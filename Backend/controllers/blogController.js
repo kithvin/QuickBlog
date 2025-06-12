07e1165bd65bfa0066f2976496b09de0c5cff218
kithvin <kithvinbandara1994@gmail.com>
2025-06-12 14:35:20 +0530

    Added API endpoints for blog list, details, update, and delete operations

--- Backend/controllers/blogController.js
+++ Backend/controllers/blogController.js
@@ -27,9 +27,9 @@ export const addBlog = async (req, res) => {
     const optimizedImageURL = imagekit.url({
       path: responce.filePath,
       transformation: [
-        { quality: 'auto' }, // Auto compression
-        { format: 'webp' }, // Convert to modern Format
-        { width: '1280' }, // Width resizing
+        { quality: "auto" }, // Auto compression
+        { format: "webp" }, // Convert to modern Format
+        { width: "1280" }, // Width resizing
       ],
     });
 
@@ -55,3 +55,47 @@ export const addBlog = async (req, res) => {
     });
   }
 };
+
+export const getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find({ isPublished: true });
+    res.json({ success: true, blogs });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+}
+
+export const getBlogById = async (req,res) => {
+  try {
+    const {blogId} = req.params;
+    const blog = await Blog.findById(blogId)
+    if(!blog){
+      res.json({ success: false, message: "Blog not found" });
+    }
+    res.json({ success: true, blog });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+}
+
+export const deleteBlogById = async (req,res) => {
+  try {
+    const {id} = req.body;
+    await Blog.findByIdAndDelete(id);
+    res.json({ success: true, message: 'Blog deleted successfully' });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+}
+
+export const togglePublish = async (req, res)=>{
+  try {
+    const {id} = req.body;
+    const blog = await Blog.findById(id);
+    blog.isPublished = !blog.isPublished;
+    await blog.save();
+    res.json({ success: true, message: 'Blog Status updated' });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+}
