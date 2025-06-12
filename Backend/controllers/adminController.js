5f91aff017292321eae6f3a1e1e1facdb04a16bf
kithvin <kithvinbandara1994@gmail.com>
2025-06-12 12:17:10 +0530

    Integrated ImageKit for uploading blog images

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -6,13 +6,13 @@ export const adminLogin = async (req,res)=>{
     const {email,password} = req.body;
 
     if(email !== process.env.ADMIN_EMAIL || password !== process.env.ADMIN_PASSWORD){
-        return res.json({sucess: false, message : "Invalid Credentials"});
+        return res.json({success: false, message : "Invalid Credentials"});
     }
 
     const token = jwt.sign({email}, process.env.JWT_SECRET);
-    res.json({sucess:true,token});
+    res.json({success:true,token});
 
     } catch (error) {
-    res.json({sucess:false,message:error.message});  
+    res.json({success:false,message:error.message});  
     }
 }
--- Backend/controllers/blogController.js
+++ Backend/controllers/blogController.js
@@ -1,17 +1,57 @@
+import fs from "fs";
+import imagekit from "../configs/imageKit.js";
+import Blog from "../models/Blog.js";
+
 export const addBlog = async (req, res) => {
   try {
-    const { title, subTitle, description, category, isPublish } = JSON.parse(
+    const { title, subTitle, description, category, isPublished } = JSON.parse(
       req.body.blog
     );
     const imageFile = req.file;
 
     // Check if all fields are present
-
-    if(!title || !description || !category || !imageFile){
-        return res.json({success:false, message:"Missing required fields"});
+    if (!title || !description || !category || !imageFile) {
+      return res.json({ success: false, message: "Missing required fields" });
     }
 
-    
+    const fileBuffer = fs.readFileSync(imageFile.path);
 
-  } catch (error) {}
+    // CHANGED: Use imagekit.upload() instead of ImageKit.upload()
+    const responce = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/blogs",
+    });
+
+    // CHANGED: Use imagekit.url() instead of ImageKit.url()
+    const optimizedImageURL = imagekit.url({
+      path: responce.filePath,
+      transformation: [
+        { quality: 'auto' }, // Auto compression
+        { format: 'webp' }, // Convert to modern Format
+        { width: '1280' }, // Width resizing
+      ],
+    });
+
+    const image = optimizedImageURL;
+
+    await Blog.create({
+      title,
+      subTitle,
+      description,
+      category,
+      image,
+      isPublished,
+    });
+
+    res.json({
+      success: true,
+      message: "Blog added successfully",
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
